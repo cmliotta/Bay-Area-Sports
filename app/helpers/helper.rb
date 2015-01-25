bddee1d57e2f86d4435e3d7edcc4b7cef0446606
cmliotta <cmliotta@msn.com>
2015-01-24 19:00:51 -0800

    got token hidden

--- app/helpers/helper.rb
+++ app/helpers/helper.rb
@@ -6,7 +6,7 @@ helpers do
   def request_api_info(title, venue)
     url = "https://api.stubhub.com/search/catalog/events/v2"
 
-    request = HTTParty.get(url, query: {title: title, venue: venue}, headers: {"Authorization" => "Bearer moan6xrBdVlrksIZkPkJ5qTcPl0a"})
+    request = HTTParty.get(url, query: {title: title, venue: venue}, headers: {"Authorization" => "Bearer " + ENV['APPLICATION_TOKEN']})
 
   pp request
 
